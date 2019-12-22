2a317d88e71c4a57eef963ba90a1b0adee758639
Voigt, Martin <martin.voigt1@mailbox.tu-dresden.de>
2019-12-22 20:59:57 +0100

    Refactoring and added documentation

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import urllib
 
 from anki.hooks import addHook, wrap
 from aqt.utils import showInfo
@@ -8,6 +7,7 @@ from aqt import mw
 
 import os
 import re
+from .typing import List
 
 from . import parse_ipa
 
@@ -30,7 +30,11 @@ select_elm = ("""<select onchange='pycmd("shLang:" +"""
               """style='vertical-align: top;'>{}</select>""")
 
 
-def paste_ipa(editor):
+def paste_ipa(editor: Editor) -> None:
+    """ Paste IPA transcription into the IPA field of the Anki editor.
+
+    :param editor: Anki editor window
+    """
     lang_alias = editor.ipa_lang_alias
     note = editor.note
 
@@ -62,75 +66,101 @@ def paste_ipa(editor):
     editor.web.eval("focusField(%d);" % editor.currentField)
 
 
-def strip_list(list_):
+def strip_list(list_: List[str]) -> List[str]:
+    """ Removes HTML code from list elements.
+
+    :param list_: list of IPA transcriptions
+    :return: cleaned list (no HTML code)
+    """
     codes = ["nbsp", "i", "b", "u"]
     return [element for element in list_ if element not in codes]
 
 
-def get_deck_name(mw):
+def get_deck_name(main_window: mw) -> str:
+    """ Get the name of the current deck.
+
+    :param main_window: main window of Anki
+    :return: name of selected deck
+    """
     try:
-        deck_name = mw.col.decks.current()['name']
+        deck_name = main_window.col.decks.current()['name']
     except AttributeError:
         # No deck opened?
         deck_name = None
     return deck_name
 
 
-def get_default_lang(mw):
+def get_default_lang(main_window: mw) -> str:
+    """ Get the IPA default language.
+
+    :param main_window: main window of Anki
+    :return: default IPA language for Anki or Anki deck
+    """
     lang = CONFIG['lang']
     if CONFIG['defaultlangperdeck']:
-        deck_name = get_deck_name(mw)
+        deck_name = get_deck_name(main_window)
         if deck_name and deck_name in CONFIG['deckdefaultlang']:
             lang = CONFIG['deckdefaultlang'][deck_name]
     return lang
 
 
-def on_setup_buttons(buttons, editor):
+def on_setup_buttons(buttons: List[str], editor: Editor) -> List[str]:
+    """ Add Addon button and Addon combobox to card editor.
+
+    :param buttons: HTML codes of the editor buttons (e.g. for bold, italic, ...)
+    :param editor: card editor object
+    :return: updated list of buttons
+    """
+    # add HTML button
     button = editor.addButton(ICONPATH, "IPA", paste_ipa)
     buttons.append(button)
 
-    # HTML "combobox"
+    # create list of language options
     previous_lang = get_default_lang(mw)
+    options = [f"""<option>{previous_lang}</option>"""]  # first entry is the last selection
 
-    option_str = """<option>{}</option>"""
-    options = []
+    options += [
+        f"""<option>{language}</option>"""
+        for language in sorted(LANGUAGES_MAP.keys(), key=str.lower)
+        if language != previous_lang
+    ]
 
-    selection = sorted(LANGUAGES_MAP.keys(), key=str.lower)
-
-    options.append(option_str.format(previous_lang))
-    for lang in selection:
-        if option_str.format(lang) not in options:
-            options.append(option_str.format(lang))
-
+    # add HTML combobox
     combo = select_elm.format("".join(options))
     buttons.append(combo)
 
     return buttons
 
 
-def set_default_lang(mw, lang):
+def set_default_lang(main_window: mw, lang: str) -> None:
+    """ Set new IPA default language.
+
+    :param main_window: main window of Anki
+    :param lang: new default language
+    """
     CONFIG['lang'] = lang  # Always update the overall default
     if CONFIG['defaultlangperdeck']:
-        deck_name = get_deck_name(mw)
+        deck_name = get_deck_name(main_window)
         if deck_name:
             CONFIG['deckdefaultlang'][deck_name] = lang
 
 
-def on_ipa_language_select(editor, lang):
-    try:
-        alias = LANGUAGES_MAP[lang]
-    except KeyError as e:
-        print(e)
-        showInfo("mist")
-        editor.ipa_lang_alias = ""
-        return False
+def on_ipa_language_select(editor: Editor, lang: str):
+    """ Set new default IPA language.
+
+    :param editor: Anki editor window
+    :param lang: name of selected language
+    """
+    alias = LANGUAGES_MAP[lang]
     set_default_lang(mw, lang)
     editor.ipa_lang_alias = alias
 
 
-def init_ipa(editor, *args, **kwargs):
-    # Get the last selected language (or the default language if the user
-    # has never chosen any)
+def init_ipa(editor: Editor, *args, **kwargs):
+    """ Get the last selected/default IPA language.
+
+    :param editor: Anki editor window
+    """
     previous_lang = get_default_lang(mw)
     editor.ipa_lang_alias = LANGUAGES_MAP.get(previous_lang, "")
 
